8f0a06986588744c04c47fb7d11a2e728812b40a
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-11-20 20:53:11 +0800

    Add files via upload

---
+++ simpleOperations.java
@@ -0,0 +1,11 @@
+public class simpleOperations {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
