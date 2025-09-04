cbb9a2af295964369b51d1e0f698c419a878f294
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-09-04 13:18:58 +0800

    Working on utility methods

--- OOP/ZooManagementSystem/animals/Kangaroo.java
+++ OOP/ZooManagementSystem/animals/Kangaroo.java
@@ -2,8 +2,8 @@ package OOP.ZooManagementSystem.animals;
 
 import OOP.ZooManagementSystem.interfaces.Viviparous;
 
-public class Kangaroo implements Mammal, Viviparous {
-    void regulateBodyTemperature() {
-        System.out.println("* regulating body temperature... *");
-    }
-}
+//public class Kangaroo implements Mammal, Viviparous {
+//    void regulateBodyTemperature() {
+//        System.out.println("* regulating body temperature... *");
+//    }
+//}
---
+++ OOP/utility/ConsoleUtils.java
@@ -0,0 +1,58 @@
+package OOP.utility;
+
+// IMPORTS
+import java.util.Scanner;
+
+public class ConsoleUtils {
+    // ATTRIBUTES
+    static Scanner scanner = new Scanner(System.in); // Create an instance of 'Scanner'
+
+    // METHODS
+    // [METHOD]: Prompts the user to press any key to continue
+    public static void pressToContinue() {
+        System.out.println("Press any key to continue...");
+        scanner.nextLine();
+    }
+
+    // [METHOD]: Display error message to the console
+    public static void errorMessage(String message, int msDelay) {
+        System.out.println("ERROR: " + message);
+        
+        try {
+            Thread.sleep(msDelay);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // [METHOD]: "Clear" the screen by pushing old content out of view
+    public static void clearScreen() {
+        for (int i = 0; i < 50; ++i) {
+            System.out.println();
+        }
+    }
+
+    // [METHOD]: Display a {length}-wide line format using {symbol}
+    public static void displayFormat(char symbol, int length) {
+        for (int i = 0; i < length; ++i) {
+            System.out.print(symbol);
+        } System.out.println();
+    }
+
+    // [METHOD]: Display the string /w delay
+    public static void animatedPrint(String str, int msDelay) {
+        for (char c : str.toCharArray()) {
+            System.out.print(c);
+
+            try {
+                Thread.sleep(msDelay);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        animatedPrint("The quick brown fox jumps over the fence", 20);
+    }
+}
